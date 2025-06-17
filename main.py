f3050742bf3e8aedc4a7ab1ce13a21f44e860890
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-06-17 05:10:20 -0400

    Added input sys.argv, no input will result in error code and exit.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai 
+from google import genai
+
+try:
+    program_input = sys.argv[1]
+except:
+    raise SystemExit(1)
+
 
 client = genai.Client(api_key=api_key)
 
-client_response = client.models.generate_content(model='gemini-2.0-flash-001',contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+client_response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=program_input)
 
 
 print(client_response.text)
