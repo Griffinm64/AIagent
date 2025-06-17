55eacb0ad686269e929ac83af9408c9d3e935339
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-06-17 05:22:19 -0400

    Added -verbose flag feature. Foundational message history handling

--- main.py
+++ main.py
@@ -5,20 +5,34 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
+#accept user input, exit if nothing passed
 try:
-    program_input = sys.argv[1]
+    user_prompt = sys.argv[1]
 except:
     raise SystemExit(1)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt) ]),
+]
 
+
+#calls api key from stored value
 client = genai.Client(api_key=api_key)
 
+#calls llm model and passes previous input
 client_response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=program_input)
+    contents=messages)
 
 
+#displays result. If verbose flag is passed include usage tokens.
 print(client_response.text)
-print(f"Prompt tokens: {client_response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {client_response.usage_metadata.candidates_token_count}")
+try:
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {client_response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {client_response.usage_metadata.candidates_token_count}")
+except:
+    pass
